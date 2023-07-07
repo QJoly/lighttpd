2e12d7e6cc80c5414ac5ca56a3f35d3561bbff6a
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-07-07 21:18:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 LOGDIR="/var/log/lighttpd"
 touch $LOGDIR/lighttpd.log
 chmod a+rw $LOGDIR/lighttpd.log
+chmod a+r -r /var/www/localhost/htdocs
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf &
 tail -f $LOGDIR/lighttpd.log
